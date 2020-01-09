ab2a9b69ce57d700e8342b6cc9e34bd50915c3fc
Abu Sohib <abushoaib73@gmail.com>
2020-01-09 16:13:25 +0600

    Basic plugin structure  created.

---
+++ wp-migrate-and-duplicate-everything.php
@@ -0,0 +1,15 @@
+<?php
+/*
+  Plugin Name: WP Migrate and Duplicate Everything
+  Description: This plugins will help you to duplicate or clone your wordpress post and pages . 
+  Version: 1.0
+  Author: Kauniaweb
+  Author URI: https://www.kauniaweb.com
+  Contributors: Rashidul islam , Piash , Prokash , Moon 
+  Text Domain: esig
+  Domain Path:       /languages
+  License/Terms and Conditions: https://www.approveme.com/terms-conditions/
+  License/Terms of Use: https://www.approveme.com/terms-of-use/
+  Privacy Policy: https://www.approveme.com/privacy-policy/
+ */
+
